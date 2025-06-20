4dbde26eb7631c753f5305df4755bf3b5797d6a5
fanstar900 <fanstar900@gmail.com>
2025-06-20 10:56:41 +0530

    for deployement

--- backend/index.js
+++ backend/index.js
@@ -319,8 +319,9 @@ app.post("/search-notes", authenticateToken, async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 module.exports = app;
+
